d2248b8557001f64a6c99230f7dcdc7f16995cc8
Kirstargazer <105673623+Kirstargazer@users.noreply.github.com>
2023-09-04 16:53:15 +0300

    Add files via upload

---
+++ Lesson_1.py
@@ -0,0 +1,18 @@
+#message printing
+print ('I Love school 334!')
+
+#variable is a container for a value.
+#First data type in Python - STRING
+
+name1 = 'Tom'
+print (name1)
+
+name2 = "Mike"
+print (name2)
+
+#We can combine strings to strings only
+print ('Hello '+ name1)
+print (name1+name2)
+
+#To check data type we should use type function
+print (type (name1))
